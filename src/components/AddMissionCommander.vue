3f3fd05e05b293328f4f53010fb6c37d9cd24bd1
Julieta Delgado <julidmeza@gmail.com>
2022-05-07 22:47:48 -0500

    Add component AddMissionCommander.vue to add a mission commander

---
+++ src/components/AddMissionCommander.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="submit-form">
+    <div v-if="!submitted">
+    <h3>Nuevo Mission Commander</h3>
+      <div class="form-group">
+        <label for="title">Nombre </label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          required
+          v-model="missionCommander.name"
+          name="title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Username</label>
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          required
+          v-model="missionCommander.username"
+          name="username"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Main stack</label>
+        <input
+          type="text"
+          class="form-control"
+          id="mainStack"
+          required
+          v-model="missionCommander.mainStack"
+          name="mainStack"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Current Enrollment</label><br>
+        <input type="radio" id="false" value="false" v-model="currentEnrollmentPicked">
+        <label for="false"> False</label>
+        <br>
+        <input type="radio" id="true" value="true" v-model="currentEnrollmentPicked">
+        <label for="true"> True</label>
+        <br>
+        <span>Eligió: {{ currentEnrollmentPicked }}</span>
+      </div>
+      <div class="form-group">
+        <label for="title">Has Azure Certification</label><br>
+        <input type="radio" id="falseCertification" value="false" v-model="hasAzureCertificationPicked">
+        <label for="falseCertification"> False</label>
+        <br>
+        <input type="radio" id="trueCertification" value="true" v-model="hasAzureCertificationPicked">
+        <label for="trueCertification"> True</label>
+        <br>
+        <span>Eligió: {{ hasAzureCertificationPicked }}</span>
+      </div>
+      <button @click="saveMissionCommander" class="btn btn-success">Agregar</button>
+    </div>
+    <div v-else>
+      <h4> Mission Commander creado exitosamente. </h4>
+      <button class="btn btn-success" @click="newMissionCommander">Add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MissionCommanderService from "../services/MissionCommanderService";
+export default {
+  name: "add-mission-commander",
+  data() {
+    return {
+      missionCommander: {
+        id: null,
+        name: "",
+        username: "",
+        mainStack: "",
+        currentEnrollment: false,
+        hasAzureCertification: false
+      },
+      submitted: false,
+      currentEnrollmentPicked: "false",
+      hasAzureCertificationPicked: "false"
+    };
+  },
+  methods: {
+    saveMissionCommander() {
+      var data = {
+        name: this.missionCommander.name,
+        username: this.missionCommander.username,
+        mainStack: this.missionCommander.mainStack,
+        currentEnrollment: this.missionCommander.currentEnrollment,
+        hasAzureCertification: this.missionCommander.hasAzureCertification
+      };
+      MissionCommanderService.create(data)
+        .then(response => {
+          this.missionCommander.id = response.data.id;
+          this.submitted = true;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    newMissionCommander() {
+      this.submitted = false;
+      this.missionCommander = {};
+    }
+  }
+};
+</script>
