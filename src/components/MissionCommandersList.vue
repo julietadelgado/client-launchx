86f9d534e354e164ea90b344cc7967b477977c74
Julieta Delgado <julidmeza@gmail.com>
2022-05-07 22:17:51 -0500

    Add MissionCommanderList.vue component to show all mission Commanders

---
+++ src/components/MissionCommandersList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4> Mission Commanders Inscritos </h4>
+      <ul class="list-group">
+        <li class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(missionCommander, index) in missionCommanders"
+          :key="index"
+          @click="setActiveMissionCommander(missionCommander, index)"
+        >
+          {{ missionCommander.username }}
+        </li>
+
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentMissionCommander">
+        <h4>Mission Commander</h4>
+        <div>
+          <br><label><strong>Nombre:</strong></label> {{ currentMissionCommander.name}}
+          <br><label><strong>Username:</strong></label> {{ currentMissionCommander.username}}
+          <br><label><strong>Mission:</strong></label> {{ currentMissionCommander.mission}}
+        </div>
+        <router-link :to="'/mission-commander/' + currentMissionCommander.id" class="btn btn-info"> Editar</router-link>
+      </div>
+      <div v-else>
+        <br />
+        <p> Selecciona un Mission Commander.</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import MissionCommanderService from "../services/MissionCommanderService";
+
+export default {
+  name: "mission-commander-list",
+  data() {
+    return {
+      tutorials: [],
+      missionCommanders: [],
+      currentTutorial: null,
+      currentMissionCommander: null,
+      currentIndex: -1,
+      title: "",
+      missionCommanderId: ""
+    };
+  },
+  methods: {
+    getAllMissionCommanders(){
+      MissionCommanderService.getAll()
+        .then(response => {
+          this.missionCommanders = response.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    setActiveMissionCommander(missionCommander, index) {
+      this.currentMissionCommander= missionCommander;
+      this.currentIndex = missionCommander? index : -1;
+    }
+  },
+  mounted() {
+    this.getAllMissionCommanders();
+  }
+};
+</script>
